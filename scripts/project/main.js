7504b9f75c73305f6d61c78f3f675a8065eb0e87
Puny-D <punyswing@163.com>
2025-01-23 15:47:13 +0800

    恢复vConsole的导入以支持调试功能

--- scripts/project/main.js
+++ scripts/project/main.js
@@ -1,6 +1,6 @@
 // import "https://telegram.org/js/telegram-web-app.js?56"
-// import "https://unpkg.com/vconsole@latest/dist/vconsole.min.js"
-// var vConsole = new window.VConsole();
+import "https://unpkg.com/vconsole@latest/dist/vconsole.min.js"
+var vConsole = new window.VConsole();
 
 // window.waitMessageInstance =  awaitForMessage();
 
