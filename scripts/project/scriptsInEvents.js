974556638135642b182da9663870e5bd203b2ce6
Puny-D <punyswing@163.com>
2025-02-05 16:26:10 +0800

    更新对象引用表和事件脚本，添加新的事件处理函数，优化代码结构，清理冗余逻辑

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -1,150 +1,200 @@
 function getHashParam(param) {
-  const hash = window.location.hash.substring(1); // 去除 `#`
-  const params = new URLSearchParams(hash);
-  return params.get(param);
+	const hash = window.location.hash.substring(1); // 去除 `#`
+	const params = new URLSearchParams(hash);
+	return params.get(param);
 }
 
+
+
 const scriptsInEvents = {
-  async Maineventsheet_Event4_Act1(runtime, localVars) {
-    console.log("server complate");
-  },
 
-  async Maineventsheet_Event5_Act1(runtime, localVars) {
-    console.error("server error");
-  },
+	async Maineventsheet_Event2_Act1(runtime, localVars)
+	{
+		console.log("severStartInfo:start");
+	},
 
-  async Maineventsheet_Event16_Act1(runtime, localVars) {},
+	async Maineventsheet_Event2_Act13(runtime, localVars)
+	{
+		console.log("severStartInfo:end");
+	},
 
-  async Maineventsheet_Event18_Act1(runtime, localVars) {
-    const search = new URLSearchParams(location.search);
-    let initData = search.get("initData");
-    initData = initData && atob(initData);
+	async Maineventsheet_Event3_Act1(runtime, localVars)
+	{
+		console.log("gameplayer:start");
+	},
 
-    let api = search.get("api");
-    api = api && atob(api);
+	async Maineventsheet_Event4_Act1(runtime, localVars)
+	{
+		console.log("server complate")
+	},
 
-	console.log("initData:", initData, "api:", api);
+	async Maineventsheet_Event5_Act1(runtime, localVars)
+	{
+		console.error("server error")
+	},
 
-    if (!initData || !api) return;
+	async Maineventsheet_Event16_Act1(runtime, localVars)
+	{
+		
+	},
 
-    runtime.globalVars.serverUrl = api + "/api/hunter";
-    runtime.globalVars.tgtTooken = initData;
+	async Maineventsheet_Event18_Act1(runtime, localVars)
+	{
+		const search = new URLSearchParams(location.search);
+		let initData = search.get("initData");
+		initData = initData && atob(initData);
+		
+		let api = search.get("api");
+		api = api && atob(api);
+		
+		if (!initData || !api) return;
+		
+		runtime.globalVars.serverUrl = api + "/api/hunter";
+		runtime.globalVars.tgtTooken = initData;
+		runtime.callFunction("serverStartInfo");
+	},
 
-    console.log("initData end:", api, initData);
-    runtime.callFunction("serverStartInfo");
-  },
+	async Maineventsheet_Event22(runtime, localVars)
+	{
+		
+		window.addEventListener('hashchange', function() {
+			const activated = getHashParam('activated');
+			if (activated === '1') {
+				console.log('Activated is 1');
+				runtime.callFunction("playStateFunc", Number(activated));
+			} else if(activated==="0"){
+				console.log('Activated is not 1');
+				runtime.callFunction("playStateFunc", Number(activated));
+			}
+		});
+	},
 
-  async Maineventsheet_Event22(runtime, localVars) {
-    window.addEventListener("hashchange", function () {
-      const activated = getHashParam("activated");
-      if (activated === "1") {
-        console.log("Activated is 1");
-        runtime.callFunction("playStateFunc", Number(activated));
-      } else if (activated === "0") {
-        console.log("Activated is not 1");
-        runtime.callFunction("playStateFunc", Number(activated));
-      }
-    });
-  },
+	async Gameplayereventsheet_Event31_Act1(runtime, localVars)
+	{
+		
+		const json = runtime.objects.JSON.getFirstInstance();
+		const LevelOutline = runtime.objects.LevelOutline.getFirstInstance();
+		const MonsterPointers = runtime.objects.MonsterPointer.getPickedInstances();
+		
+		const jsonCopy = json.getJsonDataCopy();
+		
+		const chapters =  jsonCopy.gameInfo.data.chapters;
+		const monsters =  jsonCopy.gameInfo.data.monsters;
+		
+		
+		const chapter = LevelOutline.instVars.chapterCount;
+		const sub_chapter = LevelOutline.instVars.levelCount;
+		
+		
+		const chapterData = chapters.find(item=>(item.chapter === chapter&&item.sub_chapter === sub_chapter));
+		
+		if(chapterData!==undefined){
+		
+			const monsterNames = chapterData.monsters.split(",");
+			// 	const monsterNames = ["M_004","M_002","M_003"];
+			const currentMonsterNames = [];
+		
+			for(let idx = 0; idx < monsterNames.length; idx++){
+		
+				if(idx>2) break;
+		
+		
+				const monsterData = monsters.find(item=>item.monster_id === monsterNames[idx]);
+		
+				currentMonsterNames[idx] = monsterData;
+		
+			}
+			jsonCopy.currentMonsterNames = currentMonsterNames;
+			json.setJsonDataCopy(jsonCopy);
+		
+		}
+		
+	},
 
-  async Gameplayereventsheet_Event31_Act1(runtime, localVars) {
-    const json = runtime.objects.JSON.getFirstInstance();
-    const LevelOutline = runtime.objects.LevelOutline.getFirstInstance();
-    const MonsterPointers = runtime.objects.MonsterPointer.getPickedInstances();
+	async Gameplayereventsheet_Event45_Act3(runtime, localVars)
+	{
+		
+		const json = runtime.objects.JSON.getFirstInstance();
+		const LevelOutline = runtime.objects.LevelOutline.getFirstInstance();
+		const jsonCopy = json.getJsonDataCopy();
+		
+		const chapters =  jsonCopy.gameInfo.data.chapters;
+		const chapter = LevelOutline.instVars.chapterCount;
+		const sub_chapter = LevelOutline.instVars.levelCount;
+		
+		
+		
+		const maxChapter = chapters.filter(item=>item.chapter === chapter).length;
+		
+		LevelOutline.instVars.levelMaxCount = maxChapter;
+		
+	},
 
-    const jsonCopy = json.getJsonDataCopy();
+	async Gameplayereventsheet_Event70_Act1(runtime, localVars)
+	{
+		const Chest = runtime.objects.Chest.getFirstInstance();
+		
+		Chest.instVars.nextChestTime = new Date().getTime()*0.001 + 30;
+		
+		
+	},
 
-    const chapters = jsonCopy.gameInfo.data.chapters;
-    const monsters = jsonCopy.gameInfo.data.monsters;
+	async Gameplayereventsheet_Event77_Act1(runtime, localVars)
+	{
+		const Chest = runtime.objects.Chest.getFirstInstance();
+		
+		Chest.instVars.nextChestTime = new Date().getTime()*0.001 + 30;
+		
+		
+	},
 
-    const chapter = LevelOutline.instVars.chapterCount;
-    const sub_chapter = LevelOutline.instVars.levelCount;
+	async Gameplayereventsheet_Event84_Act1(runtime, localVars)
+	{
+		const Chest = runtime.objects.Chest.getFirstInstance();
+		
+		Chest.instVars.nextChestTime = new Date().getTime()*0.001 + 30;
+		
+		
+	},
 
-    const chapterData = chapters.find(
-      (item) => item.chapter === chapter && item.sub_chapter === sub_chapter
-    );
+	async Gameplayereventsheet_Event146_Act3(runtime, localVars)
+	{
+		console.log("skin")
+	},
 
-    if (chapterData !== undefined) {
-      const monsterNames = chapterData.monsters.split(",");
-      // 	const monsterNames = ["M_004","M_002","M_003"];
-      const currentMonsterNames = [];
+	async Gameplayereventsheet_Event150_Act1(runtime, localVars)
+	{
+		const Chest = runtime.objects.Chest.getFirstInstance();
+		Chest.instVars.tickTime = new Date().getTime()*0.001;
+		
+	},
 
-      for (let idx = 0; idx < monsterNames.length; idx++) {
-        if (idx > 2) break;
+	async Gameplayereventsheet_Event173_Act1(runtime, localVars)
+	{
+		console.log("Trash")
+	},
 
-        const monsterData = monsters.find(
-          (item) => item.monster_id === monsterNames[idx]
-        );
+	async Gameplayereventsheet_Event174_Act1(runtime, localVars)
+	{
+		console.log("Eq1")
+	},
 
-        currentMonsterNames[idx] = monsterData;
-      }
-      jsonCopy.currentMonsterNames = currentMonsterNames;
-      json.setJsonDataCopy(jsonCopy);
-    }
-  },
+	async Gameplayereventsheet_Event184_Act1(runtime, localVars)
+	{
+		console.log("Eq2")
+	},
 
-  async Gameplayereventsheet_Event45_Act3(runtime, localVars) {
-    const json = runtime.objects.JSON.getFirstInstance();
-    const LevelOutline = runtime.objects.LevelOutline.getFirstInstance();
-    const jsonCopy = json.getJsonDataCopy();
+	async Gameplayereventsheet_Event194_Act1(runtime, localVars)
+	{
+		console.log("Eq3")
+	},
 
-    const chapters = jsonCopy.gameInfo.data.chapters;
-    const chapter = LevelOutline.instVars.chapterCount;
-    const sub_chapter = LevelOutline.instVars.levelCount;
+	async Gameplayereventsheet_Event219_Act2(runtime, localVars)
+	{
+		console.log("Tile")
+	}
 
-    const maxChapter = chapters.filter(
-      (item) => item.chapter === chapter
-    ).length;
-
-    LevelOutline.instVars.levelMaxCount = maxChapter;
-  },
-
-  async Gameplayereventsheet_Event70_Act1(runtime, localVars) {
-    const Chest = runtime.objects.Chest.getFirstInstance();
-
-    Chest.instVars.nextChestTime = new Date().getTime() * 0.001 + 30;
-  },
-
-  async Gameplayereventsheet_Event77_Act1(runtime, localVars) {
-    const Chest = runtime.objects.Chest.getFirstInstance();
-
-    Chest.instVars.nextChestTime = new Date().getTime() * 0.001 + 30;
-  },
-
-  async Gameplayereventsheet_Event84_Act1(runtime, localVars) {
-    const Chest = runtime.objects.Chest.getFirstInstance();
-
-    Chest.instVars.nextChestTime = new Date().getTime() * 0.001 + 30;
-  },
-
-  async Gameplayereventsheet_Event146_Act3(runtime, localVars) {
-    console.log("skin");
-  },
-
-  async Gameplayereventsheet_Event150_Act1(runtime, localVars) {
-    const Chest = runtime.objects.Chest.getFirstInstance();
-    Chest.instVars.tickTime = new Date().getTime() * 0.001;
-  },
-
-  async Gameplayereventsheet_Event173_Act1(runtime, localVars) {
-    console.log("Trash");
-  },
-
-  async Gameplayereventsheet_Event174_Act1(runtime, localVars) {
-    console.log("Eq1");
-  },
-
-  async Gameplayereventsheet_Event184_Act1(runtime, localVars) {
-    console.log("Eq2");
-  },
-
-  async Gameplayereventsheet_Event194_Act1(runtime, localVars) {
-    console.log("Eq3");
-  },
-
-  async Gameplayereventsheet_Event219_Act2(runtime, localVars) {
-    console.log("Tile");
-  },
 };
 
 self.C3.ScriptsInEvents = scriptsInEvents;
+
