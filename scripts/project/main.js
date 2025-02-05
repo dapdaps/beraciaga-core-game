a1f641908db0842dee33fdf149f86fa9a3d19b3b
Puny-D <punyswing@163.com>
2025-02-05 16:28:34 +0800

    注释掉vConsole导入以禁用调试功能

--- scripts/project/main.js
+++ scripts/project/main.js
@@ -1,7 +1,7 @@
 
 // import "https://telegram.org/js/telegram-web-app.js?56"
-import "https://unpkg.com/vconsole@latest/dist/vconsole.min.js"
-var vConsole = new window.VConsole();
+// import "https://unpkg.com/vconsole@latest/dist/vconsole.min.js"
+// var vConsole = new window.VConsole();
 
 
 
